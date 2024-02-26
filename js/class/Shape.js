23a40cb78f7069c17749b1e15e12f689d57d2368
Aki Pyykkönen <aki.pyykkonen@me.com>
2024-02-27 00:21:27 +0200

    Web-sovellusten perusteet, viikko 7

---
+++ js/class/Shape.js
@@ -0,0 +1,24 @@
+export class Shape {
+    _x
+    _y
+    _lineWidth
+    _color
+
+    constructor(x, y, _lineWidth = 1, _color = 'black') {
+        this._x = x;
+        this._y = y;
+        this._lineWith = _lineWidth
+        this._color = _color
+
+    }
+
+
+set setLineWidth(lineWidth) {
+    this._lineWidth = lineWidth;
+}
+
+set setColor(color) {
+    this._color = color;
+}
+
+}
